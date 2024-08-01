3aaf607c6bf8511763f3fcb685ae02fe6d037afc
taylornich <166781921+taylornich@users.noreply.github.com>
2024-07-31 19:58:20 -0700

    Update user_operations.py

--- user_operations.py
+++ user_operations.py
@@ -8,20 +8,24 @@ def add_user():
     #borrowed_books - need to add these with the method
 
     new_user = user(name, library_id)
-    users.append(new_user)
+    users[library_id] = new_user
+
+    print(f"User '{name}' added successfully.")
 
 def view_user_details():
-    name = input("Enter the name of the user you would like to view: ")
-    if name in users:
-        user = users[name]
-        print(f"User details: User Name: {user.get_name()}, Library ID: {user.get_library_id()}")
+    user_id = input("Enter the library ID of the user you would like to view: ")
+    if user_id in users:
+        user = users[user_id]
+        print(f"User details: User Name: {user.get_name()}, Library ID: {user.get_user_id()}")
     else:
         print("User not found")
 
 def display_users():
     if users:
         for user in users.values():
-            print(f"User Name: {user.get_name()}, Library ID: {user.get_library_id()}")
-        else:
-            print("No users currently in the database.")
+            print(f"User Name: {user.get_name()}, User ID: {user.get_user_id()}")
+    else:
+        print("No users currently in the database.")
+
+
 
