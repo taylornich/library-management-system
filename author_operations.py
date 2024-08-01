e5ad6897d1acf7fda32dadbfe4b540ce107adec6
taylornich <166781921+taylornich@users.noreply.github.com>
2024-07-31 19:55:56 -0700

    Update author_operations.py

--- author_operations.py
+++ author_operations.py
@@ -3,22 +3,26 @@ from Author import author
 authors = {}
 
 def add_author():
-    name = input("Enter the name of the author: ")
+    author_name = input("Enter the name of the author: ")
     biography = input("Enter a short biography for the author or type N/A: ")
 
-    new_author = author(name, biography)
-    authors.append(new_author)
+    new_author = author(author_name, biography)
+    authors[author_name] = new_author
+    print(f"Author {new_author} added successfully.")
 
 def view_author_details():
-    name = input("Enter the name of the author you would like to view: ")
-    if name in authors:
-        author = authors[name]
-        print(f"Author details: name: {author.get_name()}, Biography: {author.get_biography()}")
+    author_name = input("Enter the name of the author you would like to view: ")
+    if author_name in authors:
+        author = authors[author_name]
+        print(f"Author details: name: {author.get_author_name()}, Biography: {author.get_biography()}")
+    else:
+        print("Author not in the database.")
 
 def display_authors():
     if authors:
         for author in authors.values():
-            print(f"Name: {author.get_name()}, Biography: {author.get_biography()}")
-        else:
-            print("No authors currently in database.")
+            print(f"Name: {author.get_author_name()}, Biography: {author.get_biography()}")
+    else:
+        print("No authors currently in database.")
+
 
