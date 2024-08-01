f1998edebad39bc4030d4d2a6096ee46a58ec49c
taylornich <166781921+taylornich@users.noreply.github.com>
2024-07-31 19:57:15 -0700

    Update genre_operations.py

--- genre_operations.py
+++ genre_operations.py
@@ -8,19 +8,21 @@ def add_genre():
     category = input("Please enter the category that this genre belongs to, fiction or nonfiction: ")
 
     new_genre = genre(name, description, category)
-    genres.append(new_genre)
+    genres[name] = new_genre
+
+    print(f"Genre '{name}' added successfully.")
 
 def view_genre_details():
     name = input("Enter the name of the genre you would like to view: ")
     if name in genres:
         genre = genres[name]
-        print(f"Genre details: Name: {genre.get_name()}, Description: {genre.get_description()}, Category: {genre.get_category()}")
+        print(f"Genre details: Name: {genre.get_genre_name()}, Description: {genre.get_description()}, Category: {genre.get_category()}")
     else:
         print("Genre not found in the database")
 
 def display_genres():
     if genres:
         for genre in genres.values():
-            print(f"Name: {genre.get_name()}, Description: {genre.get_description}, Category: {genre.get_category()}")
+            print(f"Name: {genre.get_genre_name()}, Description: {genre.get_description}, Category: {genre.get_category()}")
     else: 
         print("No genres currently in the database")
